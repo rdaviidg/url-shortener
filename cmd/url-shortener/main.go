2a7bcbc5e010b816aadfb6e10a99fccd3922d331
David R <rdaviidg@gmail.com>
2025-09-01 13:43:40 +0300

    feat: Init logger.

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,21 +1,63 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/rdaviidg/url-shortener/internal/config"
 )
 
-func main() {
-	// TODO: init config: cleanenv
-	cfg := config.MustLoad()
-	fmt.Println(cfg)
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
 
-	// TODO: init logger: slog
+func main() {
+	os.Setenv("CONFIG_PATH", "/home/qwerty/projects/go/url-shortener/config/local.yaml")
+
+	// init config: cleanenv
+	cfg := config.MustLoad()
+
+	// init logger: slog
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	// TODO: init storage: sqlite
 
 	// TODO: init router: chi, "chi render"
 
 	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelDebug},
+			),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelDebug},
+			),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelInfo},
+			),
+		)
+	}
+
+	return log
 }
