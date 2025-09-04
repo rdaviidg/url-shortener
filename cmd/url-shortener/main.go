f18b1d7e4833be9bcea3de24a2647d3f598163cc
David R <rdaviidg@gmail.com>
2025-09-04 18:52:56 +0300

    feat: Storage, SaveURL, GetURL.

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 
 	"github.com/rdaviidg/url-shortener/internal/config"
+	"github.com/rdaviidg/url-shortener/internal/lib/logger/sl"
+	"github.com/rdaviidg/url-shortener/internal/storage/sqlite"
 )
 
 const (
@@ -26,6 +28,13 @@ func main() {
 	log.Debug("debug messages are enabled")
 
 	// TODO: init storage: sqlite
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
 
 	// TODO: init router: chi, "chi render"
 
