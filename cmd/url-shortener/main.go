93e72568f4b165e466833383c719b5d5fcd870e3
David R <rdaviidg@gmail.com>
2025-09-05 10:38:49 +0300

    feat: Handler Save - обработчик запросов на сохранение URL.

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 
 	"github.com/rdaviidg/url-shortener/internal/config"
+	"github.com/rdaviidg/url-shortener/internal/http-server/handlers/url/save"
 	mwLogger "github.com/rdaviidg/url-shortener/internal/http-server/middleware/logger"
 	"github.com/rdaviidg/url-shortener/internal/lib/logger/handlers/slogpretty"
 	"github.com/rdaviidg/url-shortener/internal/lib/logger/sl"
@@ -50,6 +51,8 @@ func main() {
 	router.Use(mwLogger.New(log))
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.URLFormat)
+
+	router.Post("/url", save.New(log, storage))
 
 	// TODO: run server
 }
---
+++ internal/http-server/handlers/url/save/save.go
@@ -0,0 +1,93 @@
+package save
+
+import (
+	"errors"
+	"log/slog"
+	"net/http"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
+
+	resp "github.com/rdaviidg/url-shortener/internal/lib/api/response"
+	"github.com/rdaviidg/url-shortener/internal/lib/logger/sl"
+	"github.com/rdaviidg/url-shortener/internal/lib/random"
+	"github.com/rdaviidg/url-shortener/internal/storage"
+)
+
+type Request struct {
+	URL   string `json:"url" validate:"required,url"`
+	Alias string `json:"alias,omitempty"`
+}
+
+type Response struct {
+	resp.Response
+	Alias string `json:"alias,omitempty"`
+}
+
+// TODO: move to config if need
+const aliasLength = 6
+
+type URLSaver interface {
+	SaveURL(urlToSave string, alias string) (int64, error)
+}
+
+func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.url.save.New"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var req Request
+
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("failed to decode request body", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("failed to decode request"))
+
+			return
+		}
+
+		log.Info("request body decoded", slog.Any("request", req))
+
+		err = validator.New().Struct(req)
+		if err != nil {
+			validateErr := err.(validator.ValidationErrors)
+
+			log.Error("invalid request", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("invalid request"))
+			render.JSON(w, r, resp.ValidationError(validateErr))
+
+			return
+		}
+
+		alias := req.Alias
+		if alias == "" {
+			alias = random.NewRandomString(aliasLength)
+		}
+
+		id, err := urlSaver.SaveURL(req.URL, alias)
+		if errors.Is(err, storage.ErrURLExists) {
+			log.Info("url already exists", slog.String("url", req.URL))
+			render.JSON(w, r, resp.Error("url already exists"))
+			return
+		}
+		if err != nil {
+			log.Error("failed to add url", sl.Err(err))
+			render.JSON(w, r, resp.Error("failed to add url"))
+			return
+		}
+
+		log.Info("url added", slog.Int64("id", id))
+
+		render.JSON(w, r, Response{
+			Response: resp.OK(),
+			Alias:    alias,
+		})
+	}
+}
